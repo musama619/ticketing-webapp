3985d51cfc7f766c0890475e6c40207c6d64e814
Usama <musama6191@gmail.com>
2023-05-24 23:24:22 +0500

    mongoose connect

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
@@ -22,6 +23,17 @@ app.all("*", async (req, res, next) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("Connected to MongoDb");
+    } catch (err) {
+        console.error(err);
+    }
+};
+
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!!!!!!");
 });
+
+start();
