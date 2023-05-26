4cf3fc6ebacff409662d94add32443050ced6978
Usama <musama6191@gmail.com>
2023-05-26 23:10:20 +0500

    hashing password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,24 +1,33 @@
-import mongoose, { Document, Schema } from 'mongoose';
-
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
 // Define the interface for the User document
 export interface IUser extends Document {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 // Define the User schema
-const UserSchema = new Schema<IUser>({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+const userSchema = new Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
 });
 
-const User = mongoose.model<IUser>('User', UserSchema);
+userSchema.pre("save", async function (done) {
+    if (this.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(this.password, salt);
+        this.set("password", hash);
+    }
+    done();
+});
+
+const User = mongoose.model<IUser>("User", userSchema);
 
 export default User;
