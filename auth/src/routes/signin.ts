e403b120982f835829d834ea9d1627019562f201
Usama <musama6191@gmail.com>
2023-05-30 23:55:29 +0500

    sign-in logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { body } from "express-validator";
 import User from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 import jwt from "jsonwebtoken";
 import { validateRequest } from "../middlewares/validate-request";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -19,8 +19,33 @@ router.post(
     ],
     validateRequest,
     async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
-        res.send("called!");
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError("Invalid email or password");
+        }
+
+        const matchPassword = await bcrypt.compare(password, existingUser.password);
+
+        if(!matchPassword){
+            throw new BadRequestError('Invalid password')
+        }
+
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                emai: existingUser.email,
+            },
+            process.env.JWT_KEY!
+        );
+
+        req.session = {
+            jwt: userJwt,
+        };
+
+        res.status(200).send(existingUser);
+
     }
 );
 
